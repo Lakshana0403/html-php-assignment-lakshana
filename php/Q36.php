d5825ae7aca52ee4d6e548a2ed130ce0a3666da8
Lakshana S <slakshana92@gmail.com>
2025-09-22 22:03:37 +0530

    Create Q36.php

---
+++ php/Q36.php
@@ -0,0 +1,14 @@
+36. Create an array of fruits in PHP and display the third element.
+
+Program:
+
+<?php
+// Create an array of fruits
+$fruits = array("Apple", "Banana", "Mango", "Orange", "Grapes");
+
+// Display the third element (index starts from 0)
+echo "The third fruit is: " . $fruits[2];
+?>
+
+Output:
+The third fruit is: Mango
